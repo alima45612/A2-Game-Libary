e2ce59e2303150b4e92aabd07e2f4c3ce557ada8
alima <tamannaalima@gmail.com>
2025-05-07 13:16:11 +0100

    users able to add,view,del

---
+++ User.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private String name;
+    private String preferredPlatform;
+    private List<Game> ownedGames;
+
+    public User(String name, String preferredPlatform) {
+        this.name = name;
+        this.preferredPlatform = preferredPlatform;
+        this.ownedGames = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPreferredPlatform() {
+        return preferredPlatform;
+    }
+
+    public List<Game> getOwnedGames() {
+        return ownedGames;
+    }
+
+    public void addGame(Game game) {
+        ownedGames.add(game);
+    }
+
+    public void removeGame(Game game) {
+        ownedGames.remove(game);
+    }
+
+    @Override
+    public String toString() {
+        return name + " (Preferred Platform: " + preferredPlatform + ")";
+    }
+}
+
